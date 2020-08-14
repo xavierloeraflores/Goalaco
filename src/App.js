e6ad7f5611d0b12f46817512552d354bddf5663b
xavierloeraflores <x.loera.tech@gmail.com>
2020-08-13 20:51:59 -0700

    First  edit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import logo from "./logo.svg";
 import "./App.css";
 //Test Test 2 from Github desktop
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  );
 }
 
 export default App;
