affecfb7c55d88cd9edc563d3eb364c13dfc345a
xavierloeraflores <x.loera.tech@gmail.com>
2020-08-13 22:36:11 -0700

    Added Firebase Config

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as firebase from "firebase";
-//Do not edit
+var firebaseConfig = {
+  apiKey: "AIzaSyDwSdVqHqtr92EHTbKUfnnbDZgCzdhLlQw",
+  authDomain: "goalaco.firebaseapp.com",
+  databaseURL: "https://goalaco.firebaseio.com",
+  projectId: "goalaco",
+  storageBucket: "goalaco.appspot.com",
+  messagingSenderId: "623184374444",
+  appId: "1:623184374444:web:63d31fc483c14d187c4d09",
+  measurementId: "G-QQFJKZBG7S",
+};
+firebase.initializeApp(firebaseConfig);
+
+//Do not edit^^
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
