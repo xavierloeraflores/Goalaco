fb50396aadd0ac30965bcafc4cf3c84aacb499ad
xavierloeraflores <x.loera.tech@gmail.com>
2020-08-13 20:12:43 -0700

    test commmit from visual studio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+//Test
 function App() {
   return (
     <div className="App">
