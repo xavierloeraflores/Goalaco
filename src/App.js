2b7b3550c58c14c5d3e79be7ab9eb804b14539e3
xavierloeraflores <x.loera.tech@gmail.com>
2020-08-13 22:22:31 -0700

    Imported firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+
 //Test Test 2 from Github desktop test 3 BT :) :) :) :) :)
 //Test 3 :)
+
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import * as firebase from "firebase";
 
 ReactDOM.render(
   <React.StrictMode>
