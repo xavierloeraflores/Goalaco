acbc89690352a45780696ec207989352b736400c
xavierloeraflores <x.loera.tech@gmail.com>
2020-08-13 21:30:07 -0700

    Test comment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-//Test Test 2 from Github desktop test 3 BT :) :) :) :) :) 
+//Test Test 2 from Github desktop test 3 BT :) :) :) :) :)
+//Test 3 :)
 function App() {
   return (
     <div className="App">
