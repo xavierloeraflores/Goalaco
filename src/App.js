89d2b062556f89108b9e3b840b1b4209dd0c81e7
xavierloeraflores <x.loera.tech@gmail.com>
2020-08-15 00:20:49 -0700

    Added login above the search and cards

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <UserProvider>
+          <Application />
+        </UserProvider>
+
         <Search />
         <Cards />
       </div>
